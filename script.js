a092a38240839784841577463186f4a1c3c8ceef
memellaguno <memellaguno@gmail.com>
2020-04-25 23:10:14 -0500

    Board Generator

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-const gameElements = (() => {
-	const board = () => foo;
 
+//Gameboard array
+var boardModule = (() => {
 
-	return {
-
-	};
-})();
-
-board = function() {
 	//Select div for board
 	let boardContainer = document.querySelector("#boardContainer")
-
-	//Define square grid element size
-	let squareSize = "50px";
+	let boardArray = [];
 
 	//Apply to board container
-	for (let i = 0; i < 6; i++) {
+	const boardGenerator = () => {
+		for (let i = 0; i < 9; i++) {
 
-		//Create Square elements
-		let square = document.createElement("div")
-		square.classList.add("squareStyle")
+			//Create Square elements
+			let square = document.createElement("div")
+			square.classList.add("squareStyle")
 
 
-		//Append to board
-		boardContainer.appendChild(square);
+			//Append to board
+			boardContainer.appendChild(square);
+		}
+
 	}
 
-	//Apply size to all squares
+	return {
+		boardGenerator: boardGenerator
+	};
 
-	squareSelector = document.querySelectorAll(".squareStyle")
-	squareSelector.forEach( element => {
-		element.height = squareSize;
-	});
+})();
 
-	return console.log("Done creating board")
-}
 
-board();
+boardModule.boardGenerator();
