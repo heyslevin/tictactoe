0767592e2591829f6787f51038f85253b0c6c2b2
memellaguno <memellaguno@gmail.com>
2020-05-01 12:32:35 -0500

    Set up basic Player Factory

--- script.js
+++ script.js
@@ -4,25 +4,43 @@ var boardModule = (() => {
 
 	//Select div for board
 	let boardContainer = document.querySelector("#boardContainer")
-	let boardArray = [];
+	let boardArray = ["x","o","x","o","x","o","x","o","x"];
 
-	//Apply to board container
-	const boardGenerator = () => {
-		for (let i = 0; i < 9; i++) {
+	//Built board with for each
 
-			//Create Square elements
+	var boardGenerator = () => {
+		boardArray.forEach( element => {
 			let square = document.createElement("div")
 			square.classList.add("squareStyle")
-
+			square.innerHTML = element
 
 			//Append to board
 			boardContainer.appendChild(square);
-		}
 
+		});
 	}
 
+	const Player = (name,mark) => {
+		getname = () => name;
+		getmark = () => mark;
+
+		const welcome = person => {
+			console.log(`Welcome to ${person.getname()}`)
+		}
+
+		return {
+			getname,
+			getmark,
+			welcome
+		}
+	}
+
+	player1 = Player("player1","X");
+	player2 = Player("player2","0");
+
+
 	return {
-		boardGenerator: boardGenerator
+		boardGenerator: boardGenerator,
 	};
 
 })();
