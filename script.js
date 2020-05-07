fa01428b110d224b01587ca947a5a11ec69bb086
memellaguno <memellaguno@gmail.com>
2020-05-07 08:45:17 -0500

    Module for onclick square works with module for boardGeneration

--- script.js
+++ script.js
@@ -6,19 +6,38 @@ var boardModule = (() => {
 	let boardContainer = document.querySelector("#boardContainer")
 	let boardArray = ["x","o","x","o","x","o","x","o","x"];
 
-	//Built board with for each
+	//Module Player Selection
 
-	var boardGenerator = () => {
+	const gameFunctions = (() => {
+
+		// Onclick function for board
+		var selection = function playerSelection() {
+			this.innerHTML = "THIS SHIT WORKS"
+		}
+
+		return {
+			selection
+		};
+
+	})();
+
+	//Module: Built board with for each
+
+	const boardGenerator = () => {
+
 		boardArray.forEach( element => {
 			let square = document.createElement("div")
 			square.classList.add("squareStyle")
 			square.innerHTML = element
+			square.onclick = gameFunctions.selection;
 
 			//Append to board
 			boardContainer.appendChild(square);
 
 		});
 	}
+
+	//Module: Build players
 
 	const Player = (name,mark) => {
 		getname = () => name;
@@ -37,6 +56,8 @@ var boardModule = (() => {
 
 	player1 = Player("player1","X");
 	player2 = Player("player2","0");
+
+	//Module: Square Selector
 
 
 	return {
